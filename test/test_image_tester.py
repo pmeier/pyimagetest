3741df631a32f41d0742a8d546dfbe3a62b3e0b3
Philip Meier <gamesnmore@online.de>
2019-12-05 15:15:20 +0100

    Added comment for test image source (#2)

--- test/test_image_tester.py
+++ test/test_image_tester.py
@@ -6,6 +6,9 @@ from pyimagetest import ImageTester
 class Tester(ImageTester, unittest.TestCase):
     @property
     def default_test_image_file(self) -> str:
+        # The test image was downloaded from
+        # http://www.r0k.us/graphics/kodak/kodim15.html
+        # and is cleared for unrestricted usage
         return path.join(path.dirname(__file__), "test_image.png")
 
 
