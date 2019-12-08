49b48048fafc51923a4971955a71017c9e88a690
Philip Meier <github.pmeier@posteo.de>
2019-12-08 11:32:41 +0100

    change interface of ImageTestcase (#8)

--- pyimagetest/image_testcase.py
+++ pyimagetest/image_testcase.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Any, Union, Optional
 from collections import OrderedDict
 import numpy as np
 from .backend import ImageBackend, builtin_image_backends
@@ -24,7 +24,11 @@ class ImageTestcase(ABC):
     def default_test_image_file(self) -> str:
         pass
 
-    def load_image(self, backend: ImageBackend, file: Optional[str] = None) -> Any:
+    def load_image(
+        self, backend: Union[ImageBackend, str], file: Optional[str] = None
+    ) -> Any:
+        if isinstance(backend, str):
+            backend = self.backends[backend]
         if file is None:
             file = self.default_test_image_file
         return backend.import_image(file)
@@ -37,9 +41,14 @@ class ImageTestcase(ABC):
         image1_backend: Optional[ImageBackend] = None,
         image2_backend: Optional[ImageBackend] = None,
     ):
-        if image1_backend is None:
+        if isinstance(image1_backend, str):
+            image1_backend = self.backends[image1_backend]
+        elif image1_backend is None:
             image1_backend = self.infer_image_backend(image1)
-        if image2_backend is None:
+
+        if isinstance(image2_backend, str):
+            image2_backend = self.backends[image2_backend]
+        elif image2_backend is None:
             image2_backend = self.infer_image_backend(image2)
 
         image1 = image1_backend.export_image(image1)
@@ -50,7 +59,7 @@ class ImageTestcase(ABC):
         np.testing.assert_allclose(actual, desired, atol=mean_abs_tolerance, rtol=0.0)
 
     def infer_image_backend(self, image: Any) -> str:
-        for name, backend in self.backends.items():
+        for backend in self.backends.values():
             if image in backend:
-                return name
+                return backend
         raise RuntimeError
