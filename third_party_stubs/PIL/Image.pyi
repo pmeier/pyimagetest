7720011fa6fda94d6975cbd8019971d57b0b7b26
Philip Meier <philip.meier@th-owl.de>
2020-03-23 15:39:49 +0100

    Add third party stubs (#28)

---
+++ third_party_stubs/PIL/Image.pyi
@@ -0,0 +1,15 @@
+from typing import Any, Union, Optional
+from io import FileIO
+import pathlib
+
+class Image:
+    mode: str
+    def save(
+        self,
+        fp: Union[str, pathlib.Path, FileIO],
+        format: Optional[str] = ...,
+        **params: Any
+    ): ...
+    def show(self, title: Optional[str] = ..., command: Optional[str] = ...): ...
+
+def open(fp: Union[str, pathlib.Path, FileIO]) -> Image: ...
